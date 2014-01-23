893b019ac44628c840f755c442b79f0cb3f7a5df
Jun Lin <linjunpop@gmail.com>
2014-01-23 19:00:54 +0800

    Optimize images.

--- config.rb
+++ config.rb
@@ -12,24 +12,24 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  activate :imageoptim do |options|
-    # print out skipped images
-    options.verbose = true
-
-    # Setting these to true or nil will let options determine them (recommended)
-    options.nice = true
-    options.threads = true
-
-    # Image extensions to attempt to compress
-    options.image_extensions = %w(.png)
-
-    # compressor worker options, individual optimisers can be disabled by passing
-    # false instead of a hash
-    options.pngcrush_options  = {:chunks => ['alla'], :fix => false, :brute => false}
-    options.pngout_options = false
-    options.optipng_options   = {:level => 6, :interlace => false}
-    options.advpng_options    = {:level => 4}
-  end
+  # activate :imageoptim do |options|
+  #   # print out skipped images
+  #   options.verbose = true
+  #
+  #   # Setting these to true or nil will let options determine them (recommended)
+  #   options.nice = true
+  #   options.threads = true
+  #
+  #   # Image extensions to attempt to compress
+  #   options.image_extensions = %w(.png)
+  #
+  #   # compressor worker options, individual optimisers can be disabled by passing
+  #   # false instead of a hash
+  #   options.pngcrush_options  = {:chunks => ['alla'], :fix => false, :brute => false}
+  #   options.pngout_options = false
+  #   options.optipng_options   = {:level => 6, :interlace => false}
+  #   options.advpng_options    = {:level => 4}
+  # end
 
   # For example, change the Compass output style for deployment
   activate :minify_css
